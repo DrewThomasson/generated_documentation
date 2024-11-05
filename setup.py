413bf4ae7c95c61ab0d8183ce3f834895aba9b25
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-11-05 02:20:45 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="do_nothing_counter_package",
+    version="0.1",
+    packages=find_packages(),
+    description="A Python package that does nothing only used as a install counter lol",
+    author="Drew Thomasson",
+    author_email= "",
+    url="https://github.com/yourusername/do_nothing_package",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
